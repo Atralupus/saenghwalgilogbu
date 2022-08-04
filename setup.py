02ef9208904d0574cc684804e04a2ab9300eed2d
Atralupus <me@atralupus.com>
2022-08-04 22:10:14 +0900

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saenghwalgilogbu",
+    version="0.1.0",
+    description="saenghwalgilogbu",
+    author="Atralupus",
+    author_email="me@atralupus.com",
+    packages=find_packages(
+        where=".",
+        include=["saenghwalgilogbu*"],
+        exclude=["tests"],
+    ),
+    python_requires=">=3.6",
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
